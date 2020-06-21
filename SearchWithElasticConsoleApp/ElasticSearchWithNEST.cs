7a56ab867d149021a2372d952cd32ffc59e31296
sushilmate <sushil.mate@outlook.com>
2020-06-21 21:43:14 +0200

    Add project files.

---
+++ SearchWithElasticConsoleApp/ElasticSearchWithNEST.cs
@@ -0,0 +1,67 @@
+﻿using Nest;
+using System;
+using System.Collections.Generic;
+
+namespace SearchWithElasticConsoleApp
+{
+    public class ElasticSearchWithNEST
+    {
+        private readonly ElasticClient _client;
+        private const string MyElasticAzureCloud = "https://fd6b03c3df4e49dcba356a2e369b39d6.westeurope.azure.elastic-cloud.com:9243/";
+        public ElasticSearchWithNEST()
+        {
+            //"http://localhost:9200"
+            var connection = new ConnectionSettings(new Uri(MyElasticAzureCloud))
+                                .DefaultIndex("customer");
+            connection.BasicAuthentication("elastic", "cIBP8KZRrWgT5w7wEyTQEJuk");
+            _client = new ElasticClient(connection);
+        }
+
+        public bool CheckHealth()
+        {
+            var healthRe = new CatHealthRequest();
+            healthRe.Pretty = true;
+
+            var health = _client.Cat.Health(healthRe);
+
+            if (!health.IsValid)
+            {
+                Console.WriteLine($"ElasticSearch Instance is down - {health.OriginalException.Message}");
+                Console.ReadKey();
+                return false;
+            }
+            return true;
+        }
+
+        public ElasticSearchWithNEST(ElasticClient client)
+        {
+            _client = client;
+        }
+
+        public string IndexCustomer(Customer customer)
+        {
+            var response = _client.IndexDocument(customer);
+
+            return response.Result.ToString();
+        }
+
+        public IEnumerable<Customer> FindDocumentByCustomerName(string name)
+        {
+            var res = _client.Search<Customer>(x =>
+                                    x.From(0)
+                                    .Size(5)
+                                    .Query(q =>
+                                        q.Match(m =>
+                                            m.Field(f => f.Name == name))));
+
+            return res.Documents;
+        }
+    }
+
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
---
+++ SearchWithElasticConsoleApp/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using Elasticsearch.Net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SearchWithElasticConsoleApp
+{
+    public class Employee
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("App Started.");
+
+            var client = new ElasticLowLevelClient();
+
+            var health = client.Cat.Health<StringResponse>();
+
+            if (!health.Success)
+            {
+                Console.WriteLine($"ElasticSearch Instance is down - {health.OriginalException.Message}");
+                Console.ReadKey();
+                return;
+            }
+
+            var employee = new Employee
+            {
+                FirstName = "Sushil",
+                LastName = "Mate"
+            };
+
+            var result = client.Index<StringResponse>("employees", "1", PostData.Serializable(employee));
+
+            //result = await client.IndexAsync<StringResponse>("employees", "1", PostData.Serializable(employee));
+
+            Console.WriteLine(JToken.Parse(result.Body).ToString(Formatting.Indented));
+
+            Console.WriteLine("Bulk Copy");
+
+            Console.WriteLine(BulkCopyToElastic(client));
+
+            var searchResponse = client.Search<StringResponse>("employees", PostData.Serializable(new
+            {
+                from = 0,
+                size = 10,
+                query = new
+                {
+                    match = new
+                    {
+                        firstName = new
+                        {
+                            query = "Martijn"
+                        }
+                    }
+                }
+            }));
+
+            Console.WriteLine($"Search Result {searchResponse.Success}");
+            Console.WriteLine(searchResponse.Body.ToString(Formatting.Indented));
+
+            Console.WriteLine("*************** Search with NEST ***************");
+            var nest = new ElasticSearchWithNEST();
+
+            if (!nest.CheckHealth())
+            {
+                return;
+            }
+
+            var res = nest.IndexCustomer(new Customer() { Id = 1, Name = "Alok", Address = "Berlin" });
+
+            Console.WriteLine(res.ToString(Formatting.Indented));
+
+            Console.WriteLine("Search Customer on Elastic by NEST");
+
+            var customers = nest.FindDocumentByCustomerName("alok");
+
+            Console.WriteLine(System.Text.Json.JsonSerializer.Serialize(customers).ToString(Formatting.Indented));
+
+            Console.ReadKey();
+        }
+
+        static string BulkCopyToElastic(ElasticLowLevelClient client)
+        {
+            var people = new object[]
+            {
+                new { index = new { _index = "employees", _type = "_doc", _id = "1"  }},
+                new { FirstName = "Martijn", LastName = "Laarman" },
+                new { index = new { _index = "employees", _type = "_doc", _id = "2"  }},
+                new { FirstName = "Greg", LastName = "Marzouka" },
+                new { index = new { _index = "employees", _type = "_doc", _id = "3"  }},
+                new { FirstName = "Russ", LastName = "Cam" },
+            };
+
+            var result = client.Bulk<StringResponse>(PostData.MultiJson(people));
+
+            return JToken.Parse(result.Body).ToString(Formatting.Indented);
+        }
+    }
+
+    public static class StringExtension
+    {
+        public static string ToString(this string str, Formatting format = Formatting.Indented)
+        {
+            try
+            {
+                return JToken.Parse(str).ToString(format);
+            }
+            catch (Exception ex)
+            {
+                return str;
+            }
+        }
+    }
+}
